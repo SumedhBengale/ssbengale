84229726059768c5cbe0ee69bc6aeefa04e284b1
Sumedh Bengale <ssbengale@gmail.com>
2022-07-19 13:17:43 +0530

    Fixed Padding to the Left of the Hamburger Menu

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
   return (
     <>
         <div class="bg-[#4f4f4f] p-2 fixed w-full z-10">
-        <div class=" mx-10 flex">
+        <div class=" mx-2 flex">
           <div class="dropdown">
             <label tabindex="0" class="btn m-1 lg:hidden bg-[#4f4f4f] text-white hover:text-[#4f4f4f] hover:bg-white">
             <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" /></svg>
